1821353c7ed294523ecc5302fb32a43c76ddb639
vPartyzan <peregrim.mt@gmail.com>
2021-06-07 13:15:03 +0200

    Add Helmet

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -11,6 +12,7 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(helmet());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false }));
